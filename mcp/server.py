31f35b7f5a6c91fa6c4faa22838ebe888edbbdcb
aleemahmad0508 <aleem.ahmad0508@gmail.com>
2026-07-29 14:00:45 +0500

    v3 of file

--- mcp/server.py
+++ mcp/server.py
@@ -1,14 +1,13 @@
 from mcp.server.fastmcp import FastMCP
 
-mcp = FastMCP("devops-mcp")
+mcp = FastMCP(
+    "devops-mcp",
+    host="0.0.0.0",
+    port=8000,
+)
 
 import tools
 
 if __name__ == "__main__":
     print("Starting Kubernetes MCP Server...")
-
-    mcp.run(
-        transport="streamable-http",
-        host="0.0.0.0",
-        port=8000
-    )
+    mcp.run(transport="streamable-http")
